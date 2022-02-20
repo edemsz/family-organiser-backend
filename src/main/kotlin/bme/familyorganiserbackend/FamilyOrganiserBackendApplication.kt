22d5a89f2961cb83db940c1c6ef3195a5050a031
Virág Ádám <viragadam100@gmail.com>
2022-02-20 23:26:30 +0100

    project commit

---
+++ src/main/kotlin/bme/familyorganiserbackend/FamilyOrganiserBackendApplication.kt
@@ -0,0 +1,11 @@
+package bme.familyorganiserbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyOrganiserBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FamilyOrganiserBackendApplication>(*args)
+}
---
+++ src/test/kotlin/bme/familyorganiserbackend/FamilyOrganiserBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package bme.familyorganiserbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FamilyOrganiserBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
