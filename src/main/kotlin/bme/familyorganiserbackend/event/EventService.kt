166788f2e9b0b6d7cc6c4da2ec739c67794ace65
Virág Ádám <viragadam100@gmail.com>
2022-05-05 14:27:52 +0200

    search event by family

--- src/main/kotlin/bme/familyorganiserbackend/event/EventService.kt
+++ src/main/kotlin/bme/familyorganiserbackend/event/EventService.kt
@@ -1,8 +1,19 @@
 package bme.familyorganiserbackend.event
 
 import bme.familyorganiserbackend.abstracts.AbstractService
+import bme.familyorganiserbackend.family.FamilyService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class EventService : AbstractService<Event>() {
+    @Autowired
+    private lateinit var eventRepository: EventRepository
+    @Autowired
+    private lateinit var familyService: FamilyService
+
+    fun getAllByFamily(familyId: Long): List<Event> {
+        val f=familyService.getById(familyId)!!
+        return eventRepository.findByFamily(f)
+    }
 }
